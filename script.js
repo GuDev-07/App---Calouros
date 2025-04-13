7301f02c10be31979fd412f1cd269785b3a5ed37
Gustavo <gustavinhojesussant@gmail.com>
2025-04-13 00:32:30 -0300

    13-04-2025

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //MENU HAMBURGUER PARA A PAGINA
-const menuToggle = document.getElementById('menuToggle');
-const navbarLinks = document.querySelector('.navbar-links');
-const navLinks = document.querySelectorAll('.navbar-links a');
-const closeMenu = document.querySelector('.close-menu a'); //X que fecha
+const menuToggle = document.getElementById("menuToggle");
+const navbarLinks = document.querySelector(".navbar-links");
+const navLinks = document.querySelectorAll(".navbar-links a");
+const closeMenu = document.querySelector(".close-menu a"); //X que fecha
 
 //Alternar menu, abrir e fechar
 menuToggle.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+    navbarLinks.classList.toggle("active");
 });
 
 navLinks.forEach(link => {
@@ -16,20 +16,23 @@ navLinks.forEach(link => {
 });
 //FINALIZADO HAMBURGUER MOBILE
 
-
 //EVENTOS
 let indice = 0;
 
 function mudarSlide(direcao) {
     const carrossel = document.querySelector(".carrossel");
-    const totalEventos = document.querySelectorAll(".evento").length;
+    const eventos = document.querySelectorAll(".evento");
 
     indice += direcao;
-    if (indice >= totalEventos) indice = 0;
-    if (indice < 0) indice = totalEventos - 1;
 
-    carrossel.style.transform = `translateX(-${indice * 100}%)`;
+    if (indice >= eventos.length) indice = 0;
+    if (indice < 0) indice = eventos.length - 1;
+
+    const eventoLargura = eventos[0].offsetWidth + 20; // 20 = margem lateral
+    carrossel.style.transform = `translateX(-${indice * eventoLargura}px)`;
 }
 
-// Muda automaticamente a cada 5 segundos
-setInterval(() => mudarSlide(1), 5000);
+
+// Muda automaticamente a cada 50 segundos
+setInterval(() => mudarSlide(1), 50000);
+
